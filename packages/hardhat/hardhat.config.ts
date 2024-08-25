38a4304cb3adf5b139884cafd80219cc1cef5450
Manu <frosimanuel@gmail.com>
2024-08-24 22:51:59 -0300

    adding avalanche l0

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -47,6 +47,10 @@ const config: HardhatUserConfig = {
         enabled: process.env.MAINNET_FORKING_ENABLED === "true",
       },
     },
+    avalancheLocal: {
+      url: `https://automatic-giggle-qvw45qw77qjc4qp5-9650.app.github.dev/ext/bc/eleven1/rpc`,
+      accounts: [`0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027`], // ,
+    },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -1,32 +1,51 @@
+import { Chain } from "viem";
 import * as chains from "viem/chains";
 
 export type ScaffoldConfig = {
-  targetNetworks: readonly chains.Chain[];
+  targetNetworks: readonly Chain[];
   pollingInterval: number;
   alchemyApiKey: string;
   walletConnectProjectId: string;
   onlyLocalBurnerWallet: boolean;
 };
 
+// Define the Avalanche L1 chain object
+const avalancheL1: Chain = {
+  id: 43114, // Avalanche C-Chain ID
+  name: "eleven1",
+  nativeCurrency: {
+    name: "Test",
+    symbol: "TEST",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://automatic-giggle-qvw45qw77qjc4qp5-9650.app.github.dev/ext/bc/eleven1/rpc"],
+    },
+    public: {
+      http: ["https://automatic-giggle-qvw45qw77qjc4qp5-9650.app.github.dev/ext/bc/eleven1/rpc"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "SnowTrace",
+      url: "https://snowtrace.io",
+    },
+  },
+  testnet: false,
+};
+
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
-  // targetNetworks: [chains.polygonAmoy],
+  targetNetworks: [chains.hardhat, chains.polygonAmoy, avalancheL1],
 
   // The interval at which your front-end polls the RPC servers for new data
-  // it has no effect if you only target the local network (default is 4000)
   pollingInterval: 30000,
 
   // This is ours Alchemy's default API key.
-  // You can get your own at https://dashboard.alchemyapi.io
-  // It's recommended to store it in an env variable:
-  // .env.local for local testing, and in the Vercel/system env config for live apps.
   alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
 
   // This is ours WalletConnect's default project ID.
-  // You can get your own at https://cloud.walletconnect.com
-  // It's recommended to store it in an env variable:
-  // .env.local for local testing, and in the Vercel/system env config for live apps.
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
 
   // Only show the Burner Wallet when running on hardhat network
